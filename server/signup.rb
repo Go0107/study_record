9076e2d2fe393c987e1218ec026853c324aa38b9
rem0930 <06araki@gmail.com>
2024-01-23 22:35:36 +0900

    Improve pass

--- server/signup.rb
+++ server/signup.rb
@@ -1,22 +1,26 @@
 require 'webrick'
 require 'mysql2'
+require 'digest'
 
 $db_client = Mysql2::Client.new(
   host: 'localhost',       # 全員、localhostでOKです
   username: 'root',        # ひとまず、権限を一番持っているrootユーザーにしました
-  password: '　　　',   # 自身のmysqlのrootユーザーのパスワードをここに入力します
+  password: '',   # 自身のmysqlのrootユーザーのパスワードをここに入力します
   database: 'study_record' # 全員、study_recordでOKです（自身のPCにstudy_recordのデータベースを作成してください）
 )
 
 # ユーザ情報をデータベースに保存するメソッドを定義
 def save_user_to_database(username, password)
-
-  #ユーザをデータベースに挿入（なくてもいいかも）
-  puts "ユーザーデータをデータベースに挿入中です: #{username}, #{password}"
-
-  # 'users'テーブルにユーザデータを挿入するSQLクエリを構築し実行（How_to_Signupで解説）
-  insert_query = "INSERT INTO users (username, password) VALUES ('#{username}', '#{password}')"
-  $db_client.query(insert_query)
+  if username.empty? || password.empty?
+    puts "未入力の項目があります"
+    return false
+  else
+    # 'users'テーブルにユーザデータを挿入するSQLクエリを構築し実行（How_to_Signupで解説）
+    hashed_password = Digest::SHA256.hexdigest(password)
+    insert_query = "INSERT INTO users (username, password) VALUES (?, ?)"
+    $db_client.query(insert_query, username, password)
+    return true
+  end
 end
 
 # ポート3000でWEBrick HTTPサーバーを作成
@@ -34,19 +38,20 @@ server.mount('/assets', WEBrick::HTTPServlet::FileHandler, File.join(Dir.pwd, '.
 # '/signup'エンドポイントへのPOSTリクエストを処理
 server.mount_proc('/signup') do |req, res|
   if req.request_method == 'POST'
-    
     # リクエストボディからパラメータを解析
     params = WEBrick::HTTPUtils.parse_query(req.body)
     username = params['name']
     password = params['password']
 
     # save_user_to_databaseメソッドを呼び出してユーザデータをデータベースに挿入
-    save_user_to_database(username, password)
-
-    # サインアップの処理が成功したら'/top'のURLにリダイレクト
-    res.set_redirect(WEBrick::HTTPStatus::SeeOther, '/top')
+    if save_user_to_database(username, password)
+      # サインアップの処理が成功したら'/home'のURLにリダイレクト
+      res.set_redirect(WEBrick::HTTPStatus::SeeOther, '/home')
+    else
+      # ユーザー名またはパスワードが空の場合は前のページにリダイレクト
+      res.set_redirect(WEBrick::HTTPStatus::SeeOther, '/top')
+    end
   else
-
     # リクエストメソッドがPOSTでない場合はエラーレスポンスを設定
     res.body = '無効なリクエストです'
   end
