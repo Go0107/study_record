31bcf0007c2e574b728736fc65f08bc330b0fa75
Go0107 <srfiveu.0107@gmail.com>
2024-01-25 01:33:38 +0900

    日にちの初期値の設定

--- js/new_diary.js
+++ js/new_diary.js
@@ -1,3 +1,11 @@
 fetch("../header/after_header.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#header").innerHTML = data);
+
+
+let today = new Date();
+today.setDate(today.getDate());
+let yyyy = today.getFullYear();
+let mm = ("0"+(today.getMonth()+1)).slice(-2);
+let dd = ("0"+today.getDate()).slice(-2);
+document.getElementById("today").value=yyyy+'-'+mm+'-'+dd;
