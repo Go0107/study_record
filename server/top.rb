74628d23076d4e55fe96d7029a32810419804cb4
rem0930 <06araki@gmail.com>
2024-01-24 20:56:38 +0900

    make_top

---
+++ server/top.rb
@@ -0,0 +1,19 @@
+# top.rb
+
+require 'webrick'
+
+server = WEBrick::HTTPServer.new(Port: 3000)
+
+trap('INT') { server.shutdown }
+
+# ERBハンドラを使用してtopページを提供
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, '../pages/top.html')
+
+server.mount('/signup.html', WEBrick::HTTPServlet::ERBHandler, '../pages/signup.html')
+
+server.mount('/login.html', WEBrick::HTTPServlet::ERBHandler, '../pages/login.html')
+
+# 'assets'ディレクトリから静的なアセットを提供
+server.mount('/assets', WEBrick::HTTPServlet::FileHandler, File.join(Dir.pwd, '../assets'))
+
+server.start
